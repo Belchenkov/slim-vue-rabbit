5e7ae7786ae4aed994b9fe2b26911bed482cb7e7
Aleksey Belchenkov <u608110@gmail.com>
2021-04-30 13:31:51 +0400

    Added tests fixtures

--- api/tests/Feature/WebTestCase.php
+++ api/tests/Feature/WebTestCase.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Api\Test\Feature;
 
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -47,5 +51,24 @@ class WebTestCase extends TestCase
     private function container(): ContainerInterface
     {
         return require 'config/container.php';
+    }
+
+    protected function loadFixtures(array $fixtures): void
+    {
+        $container = $this->container();
+        $em = $container->get(EntityManagerInterface::class);
+        $loader = new Loader();
+
+        foreach ($fixtures as $class) {
+            if ($container->has($class)) {
+                $fixture = $container->get($class);
+            } else {
+                $fixture = new $class;
+            }
+            $loader->addFixture($fixture);
+        }
+
+        $executor = new ORMExecutor($em, new ORMPurger($em));
+        $executor->execute($loader->getFixtures());
     }
 }
